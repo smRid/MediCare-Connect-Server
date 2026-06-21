279c6f8a50f0c489f9eb853f7b31f57f3efe9ef7
smRid <omorsk911@gmail.com>
2026-06-21 11:21:50 +0600

    Add environment and CORS configuration

--- index.js
+++ index.js
@@ -1,8 +1,16 @@
 const express = require("express");
+require("dotenv").config();
+const cors = require("cors");
 
 const app = express();
 const port = process.env.PORT || 5000;
 
+app.use(
+  cors({
+    origin: process.env.CLIENT_ORIGIN?.split(",") || "*",
+    credentials: true,
+  }),
+);
 app.use(express.json());
 
 app.get("/", (_req, res) => {
